90272969792a49bfe93756405b61e737151f7c3c
Morgenthaler, Sven <Sven.Morgenthaler@vector.com>
2023-03-16 17:09:21 +0100

    SKA-13: adding run all linux script (#13)
    * introduced waiting time after starting sil-kit-registry
    * adding trap for killing child processes if user aborts script execution

--- tap/demos/CANoe4SW_SE/run_all.sh
+++ tap/demos/CANoe4SW_SE/run_all.sh
@@ -2,6 +2,9 @@
 scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 silKitDir=/home/vector/SilKit/SilKit-4.0.19-ubuntu-18.04-x86_64-gcc/
 
+# cleanup trap for child processes 
+trap 'kill $(jobs -p); exit' EXIT SIGHUP;
+
 if [ ! -d "$silKitDir" ]; then
     echo "The var 'silKitDir' needs to be set to actual location of your SilKit"
     exit 1
@@ -13,7 +16,7 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-#Make a temporary fifo to use as std:cin which is not fd#0 (this shell's std:cin) to prevent unintended closure of the background-launched processes
+# Make a temporary fifo to use as std:cin which is not fd#0 (this shell's std:cin) to prevent unintended closure of the background-launched processes
 tmp_fifo=$(mktemp -u)
 mkfifo $tmp_fifo
 exec 3<>$tmp_fifo
@@ -21,11 +24,11 @@ rm $tmp_fifo
 
 <&3 $silKitDir/SilKit/bin/sil-kit-registry --listen-uri 'silkit://0.0.0.0:8501' &> $scriptDir/sil-kit-registry.out &
 
+# give sil-kit-registry time for startup
+sleep 1
+
 <&3 $scriptDir/../start_adapter_and_ping_demo.sh &> $scriptDir/start_adapter_and_ping_demo.out &
 
 <&3 $scriptDir/../../../build/bin/SilKitDemoEthernetIcmpEchoDevice &> $scriptDir/SilKitDemoEthernetIcmpEchoDevice.out &
 
 $scriptDir/run.sh
-
-#cleanup of launched processes
-kill -s 15 $(jobs -p)
