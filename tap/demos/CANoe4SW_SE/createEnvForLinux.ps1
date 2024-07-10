28cc5bf2cd517fe2b3d9559162cacc87191ab4e0
Aubry, Kyllian <Kyllian.Aubry@vector.com>
2024-07-10 10:22:13 +0200

    SKA-565: update createEnvForLinux.ps1 (#87)

--- tap/demos/CANoe4SW_SE/createEnvForLinux.ps1
+++ tap/demos/CANoe4SW_SE/createEnvForLinux.ps1
@@ -1,3 +1,7 @@
+param (
+    [string]$remote_ip
+)
+
 #configure error handling
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 3
@@ -11,3 +15,29 @@ $canoe4sw_se_install_dir = $env:CANoe4SWSE_InstallDir64
 #compile test unit
 & $canoe4sw_se_install_dir/test-unit-make.exe "$PSScriptRoot/../tests/Testing_TAP_ping_demo.vtestunit.yaml" -e "$PSScriptRoot/Default.venvironment" -o "$PSScriptRoot"
 
+$ipv4Pattern = '^(?:(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(?!$)|$)){4}$' # regex that matches an IPv4 addresses
+
+$ipAddressNotEmpty = -not [string]::IsNullOrEmpty($remote_ip)
+
+if($ipAddressNotEmpty)
+{
+	if($remote_ip -match $ipv4Pattern)
+	{
+		# remote machine and paths configuration
+		$remote_username="vector"
+		$remote_SKA_base_path="/home/$remote_username/vfs/sil-kit-adapters-tap"
+		$artifacts_subdirectory = "tap/demos/CANoe4SW_SE"
+		$remote_full_path = Join-Path -Path $remote_SKA_base_path -ChildPath $artifacts_subdirectory
+
+		#copy artifacts to VM
+		Write-Host "Copying artifacts to [${remote_username}@${remote_ip}:${remote_full_path}]..."
+		scp -r $PSScriptRoot/Default.venvironment $PSScriptRoot/*.vtestunit "${remote_username}@${remote_ip}:${remote_full_path}"
+		Write-Host "Done."
+	}
+	else {
+		Write-Host "[Warning] Passed IP address does not have a right format, nothing will be copied to remote."
+	}
+}
+else {
+	Write-Host "[Info] No IP address has been passed, nothing will be copied to remote."
+}
