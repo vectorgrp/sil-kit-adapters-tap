7285ab3cc478d0503537271ef71838b26cc1e46e
Aubry, Kyllian <Kyllian.Aubry@vector.com>
2024-07-11 08:53:36 +0200

    SKA-567: remove TAP device after exiting the start_adapter script (#89)

--- tap/demos/DemoLinux/start_adapter_and_ping_demo.sh
+++ tap/demos/DemoLinux/start_adapter_and_ping_demo.sh
@@ -7,7 +7,7 @@ mkdir -p $logDir # if it does not exist, create it
 echo "Setting up network and ping the echo demo device via SIL Kit Adapter TAP..."
 
 # cleanup trap for child processes 
-trap 'kill $(jobs -p); exit' EXIT SIGHUP;
+trap 'kill $(jobs -p); ip netns delete tap_demo_ns; ip tuntap del dev silkit_tap mode tap; exit' EXIT SIGHUP;
 
 # check if user is root
 if [[ $EUID -ne 0 ]]; then
@@ -23,14 +23,12 @@ fi
 echo "Creating tap device silkit_tap"
 ip tuntap add dev silkit_tap mode tap
 
-
 echo "Starting sil-kit-adapter-tap..."
 $scriptDir/../../../bin/sil-kit-adapter-tap --configuration $scriptDir/../SilKitConfig_Adapter.silkit.yaml &> $logDir/sil-kit-adapter-tap.out &
 sleep 1 # wait 1 second for the creation/existense of the .out file
 
 timeout 30s grep -q 'Press CTRL + C to stop the process...' <(tail -f $logDir/sil-kit-adapter-tap.out -n +1) || { echo "[error] Timeout reached while waiting for sil-kit-adapter-tap to start"; exit 1; }
 echo "sil-kit-adapter-tap has been started"
-
 
 # Hint: It is important to establish the connection to the the adapter before moving the tap device to its separate namespace
 echo "Moving tap device 'silkit_tap' to network namespace 'tap_demo_ns'"
