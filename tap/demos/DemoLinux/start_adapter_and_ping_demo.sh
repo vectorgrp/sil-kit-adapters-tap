f862b259d0ed319e1a437d801287b3cb6630c6ad
Aubry, Kyllian <Kyllian.Aubry@vector.com>
2025-03-04 14:22:57 +0100

    SKA-724: fix start adapter script (#132)

--- tap/demos/DemoLinux/start_adapter_and_ping_demo.sh
+++ tap/demos/DemoLinux/start_adapter_and_ping_demo.sh
@@ -17,36 +17,24 @@ child_processes=""
 
 # cleanup function called on exit
 cleanup() {
-  kill $child_processes > /dev/null 2>&1
+  echo $child_processes | xargs kill
   
   if ip netns list | grep -q tap_demo_ns; then
     ip netns delete tap_demo_ns
   fi
 
   ip tuntap del dev silkit_tap mode tap
+
   rm -f "$scriptDir/temp_fifo"
 }
 
 # cleanup trap 
-trap 'cleanup; exit' EXIT HUP INT TERM;
+trap 'trap "" EXIT; cleanup; exit' EXIT HUP INT TERM;
 
 echo "[info] Setting up network and ping the echo demo device via SIL Kit Adapter TAP..."
 
-# check if running on Ubuntu, openSUSE or Android system
-netns_path=
-if cat /proc/version | grep -q -i "android"; then
-  netns_path="/mnt/run"
-elif cat /proc/version | grep -q -i -e "ubuntu" -e "SUSE"; then
-  netns_path="/var/run/netns"
-else
-  echo "[error] Not supported OS. Please run this script on Ubuntu or Android system."
-  exit 1
-fi
-
 echo "[info] Recreating tap_demo_ns network namespace"
-if [ -f "${netns_path}/tap_demo_ns" ]; then
-    ip netns delete tap_demo_ns
-fi
+ip netns delete tap_demo_ns > /dev/null 2>&1
 
 echo "[info] Creating tap device silkit_tap"
 ip tuntap add dev silkit_tap mode tap
@@ -56,12 +44,11 @@ echo "[info] Starting sil-kit-adapter-tap..."
 $scriptDir/../../../bin/sil-kit-adapter-tap --configuration $scriptDir/../SilKitConfig_Adapter.silkit.yaml > $logDir/sil-kit-adapter-tap.out &
 child_processes="$child_processes $!"
 
-sleep 2 # wait 1 second for the creation/existense of the .out file
+sleep 1 # wait 1 second for the creation/existense of the .out file
 
 mkfifo "$scriptDir/temp_fifo"
 
 tail -n +1 -f "$logDir/sil-kit-adapter-tap.out" > "$scriptDir/temp_fifo" &
-child_processes="$child_processes $!"
 
 timeout 30s grep -q 'Press CTRL + C to stop the process...' "$scriptDir/temp_fifo" || { echo "[error] Timeout reached while waiting for sil-kit-adapter-tap to start"; exit 1; }
 echo "[info] sil-kit-adapter-tap has been started"
@@ -77,5 +64,17 @@ ip -netns tap_demo_ns addr add 192.168.7.2/16 dev silkit_tap
 ip -netns tap_demo_ns link set silkit_tap up
 
 echo "[info] Starting to ping the echo device..."
+# check if running on Ubuntu, openSUSE or Android system
+netns_path=
+if [ -e "/var/run/netns/tap_demo_ns" ]; then
+  netns_path="/var/run/netns"
+elif [ -e "/mnt/run/tap_demo_ns" ]; then
+  netns_path="/mnt/run"
+else
+  echo "[error] Not supported OS. Please run this script on Ubuntu or Android system."
+  exit 1
+fi
 # Hint: The command 'ping 192.168.7.35' can be replaced by any other command or application which should send/receive data to/from SIL Kit via silkit_tap
-sh -c "nsenter --net=${netns_path}/tap_demo_ns ping 192.168.7.35"
+nsenter -F --net=${netns_path}/tap_demo_ns ping 192.168.7.35 &
+child_processes="$child_processes $!"
+wait
